8b9270e213202a9962211bfa81e507f1cf7fc79e
Bryan Matthews <bmatthews@peninsulaconsulting.ca>
2026-05-19 13:08:39 -0700

    Basic project compiling with esp-idf vscode plugin

--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,17 @@
 #include "esp_flash.h"
 #include "esp_err.h"
 #include "esp_log_custom.h"
-#include "nvs_flash.h"
+#include <nvs_flash.h>
 
-#include <IntervalTimer.h>
-#include "AppManager.h"
-#include "LedStripDriver.h"
-#include <BleConnection.h>
-#include "WifiConnection.h"
-#include "TelnetServer.h"
-#include "YRShellEsp32.h"
+// #include <IntervalTimer.h>
+// #include "AppManager.h"
+// #include "LedStripDriver.h"
+// #include <BleConnection.h>
+// #include "WifiConnection.h"
+// #include "TelnetServer.h"
+//#include "YRShellEsp32.h"
 
-#include "esp_littlefs.h"
+//#include "esp_littlefs.h"
 
 #define YRSHELL_ON_TELNET
 #define LOCAL_LOG_BUFFER_SIZE 8192
@@ -26,108 +26,108 @@ static char s_appVersion[] = "0.9.0";
 static const char* TAG = "Main   ";
 TaskHandle_t xHandle = NULL;
 
-CircularQ<char, LOCAL_LOG_BUFFER_SIZE> m_logQ;
-AppManager appMgr(s_appName, s_appVersion);
-YRShellEsp32 shell;
-LedStripDriver ledStrip;
-BleConnection bleConnection;
-WifiConnection wifiConnection(&ledStrip, 7500);
-TelnetServer telnetServer;
-TelnetLogServer telnetLogServer;
+// CircularQ<char, LOCAL_LOG_BUFFER_SIZE> m_logQ;
+// AppManager appMgr(s_appName, s_appVersion);
+// YRShellEsp32 shell;
+// LedStripDriver ledStrip;
+// BleConnection bleConnection;
+// WifiConnection wifiConnection(&ledStrip, 7500);
+// TelnetServer telnetServer;
+// TelnetLogServer telnetLogServer;
 
-bool logOut(char c) {
-  static char logOverflow[] = "\r\n\nLOG DATA DROPPED\r\n\n";
-  bool ret = true;
-    if( m_logQ.spaceAvailable( 24)) {
-      m_logQ.put( c);
-    } else {
-      char *s = logOverflow;
-      ret = false;
-      m_logQ.reset();
-      while( *s != '\0') {
-        m_logQ.put( *s++);
-      }
-    }
-    return ret;
-}
-int custom_log_handler(const char* format, va_list args) {
-    // Format the message into a buffer
-    char buf[128];
-    int ret = vsnprintf(buf, sizeof(buf), format, args);
-    char *s = buf;
-    while( *s != '\0') {
-      if(!logOut( *s++)) {
-        break;
-      }
-    }
-    return ret; 
-}
+// bool logOut(char c) {
+//   static char logOverflow[] = "\r\n\nLOG DATA DROPPED\r\n\n";
+//   bool ret = true;
+//     if( m_logQ.spaceAvailable( 24)) {
+//       m_logQ.put( c);
+//     } else {
+//       char *s = logOverflow;
+//       ret = false;
+//       m_logQ.reset();
+//       while( *s != '\0') {
+//         m_logQ.put( *s++);
+//       }
+//     }
+//     return ret;
+// }
+// int custom_log_handler(const char* format, va_list args) {
+//     // Format the message into a buffer
+//     char buf[128];
+//     int ret = vsnprintf(buf, sizeof(buf), format, args);
+//     char *s = buf;
+//     while( *s != '\0') {
+//       if(!logOut( *s++)) {
+//         break;
+//       }
+//     }
+//     return ret; 
+// }
 
-bool mountLittleFs() {
-    esp_vfs_littlefs_conf_t conf = {
-        .base_path = "/littlefs",
-        .partition_label = "spiffs",
-        .partition = NULL,
-        .format_if_mount_failed = false,
-        .read_only = false,
-        .dont_mount = false,
-        .grow_on_mount = true
-    };
+// bool mountLittleFs() {
+//     esp_vfs_littlefs_conf_t conf = {
+//         .base_path = "/littlefs",
+//         .partition_label = "spiffs",
+//         .partition = NULL,
+//         .format_if_mount_failed = false,
+//         .read_only = false,
+//         .dont_mount = false,
+//         .grow_on_mount = true
+//     };
 
-    esp_err_t err = esp_vfs_littlefs_register(&conf);
-    if (err == ESP_FAIL) {
-        ESP_LOGE(TAG, "Mounting LittleFS failed! Error: %d", err);
-        return false;
-    }
-    return true;
-}
+//     esp_err_t err = esp_vfs_littlefs_register(&conf);
+//     if (err == ESP_FAIL) {
+//         ESP_LOGE(TAG, "Mounting LittleFS failed! Error: %d", err);
+//         return false;
+//     }
+//     return true;
+// }
 
 static void loop(void *pvParameters) {
     unsigned telnetPort = 23;
     unsigned telnetLogPort = 2023;
 
-    appMgr.init();
-    ledStrip.setup();
-    bleConnection.setup();
+    // appMgr.init();
+    // ledStrip.setup();
+    // bleConnection.setup();
 
-    wifiConnection.setup();
-    wifiConnection.enable();
+    // wifiConnection.setup();
+    // wifiConnection.enable();
 
-    if( telnetLogPort != 0) {
-        telnetLogServer.init( telnetLogPort);
-        telnetLogServer.enable(true);
-    }
+    // if( telnetLogPort != 0) {
+    //     telnetLogServer.init( telnetLogPort);
+    //     telnetLogServer.enable(true);
+    // }
 
-    if( telnetPort != 0) {
-        telnetServer.init( telnetPort, &shell.getInq(), &shell.getOutq());
-    }
+    // if( telnetPort != 0) {
+    //     telnetServer.init( telnetPort, &shell.getInq(), &shell.getOutq());
+    // }
 
-    shell.setLedDriver(&ledStrip);
-    shell.setAppMgr(&appMgr);
-    shell.setWifiConnection(&wifiConnection);
-    shell.setBleConnection(&bleConnection);
-    shell.setLedStrip(&ledStrip);
-    shell.init();
+    // shell.setLedDriver(&ledStrip);
+    // shell.setAppMgr(&appMgr);
+    // shell.setWifiConnection(&wifiConnection);
+    // shell.setBleConnection(&bleConnection);
+    // shell.setLedStrip(&ledStrip);
+    // shell.init();
 
     while(1) {
-        Sliceable::sliceAll( );
+        // Sliceable::sliceAll( );
 
-        bool telnetSpaceAvailable = telnetLogServer.spaceAvailable( 32);
-        bool serialSpaceAvailable = true;
-        if( m_logQ.valueAvailable() && (telnetSpaceAvailable || serialSpaceAvailable)) {
-            char c;
-            for( uint8_t i = 0; i < 32 && m_logQ.valueAvailable(); i++) {
-                c = m_logQ.get();
-                if(telnetSpaceAvailable) {
-                    telnetLogServer.put( c);
-                }
-            #ifdef YRSHELL_ON_TELNET
-                if(serialSpaceAvailable) {
-                    printf("%c", c);
-                }
-            #endif
-            }
-        }
+        // bool telnetSpaceAvailable = telnetLogServer.spaceAvailable( 32);
+        // bool serialSpaceAvailable = true;
+        // if( m_logQ.valueAvailable() && (telnetSpaceAvailable || serialSpaceAvailable)) {
+        //     char c;
+        //     for( uint8_t i = 0; i < 32 && m_logQ.valueAvailable(); i++) {
+        //         c = m_logQ.get();
+        //         if(telnetSpaceAvailable) {
+        //             telnetLogServer.put( c);
+        //         }
+        //     #ifdef YRSHELL_ON_TELNET
+        //         if(serialSpaceAvailable) {
+        //             printf("%c", c);
+        //         }
+        //     #endif
+        //     }
+        // }
 
         vTaskDelay(1);
     }
@@ -137,7 +137,7 @@ extern "C" void app_main() {
 
     ESP_ERROR_CHECK(nvs_flash_init());
 
-    esp_log_set_vprintf(custom_log_handler);
+    // esp_log_set_vprintf(custom_log_handler);
 
     esp_log_level_set("*", ESP_LOG_INFO);
     esp_log_level_set("Main   ", ESP_LOG_INFO);
@@ -151,7 +151,7 @@ extern "C" void app_main() {
 
     ESP_LOGI(TAG, "Main Startup");
 
-    mountLittleFs();
+    // mountLittleFs();
 
     uint32_t ret = xTaskCreate(&loop, "loop", 4096, NULL, 5, &xHandle);
 
